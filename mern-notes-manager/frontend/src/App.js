3957f81e44717262e1a820f3d1ae0da5ea5531dd
SamahaMunir <119500740+SamahaMunir@users.noreply.github.com>
2025-05-05 21:13:55 +0500

    Updated app.js

--- mern-notes-manager/frontend/src/App.js
+++ mern-notes-manager/frontend/src/App.js
@@ -1,16 +1,28 @@
-
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import NotesList from './components/NotesList';
-
+import AddNoteForm from './components/AddNoteForm';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const fetchNotes = async () => {
+    const res = await axios.get('http://localhost:5000/api/notes');
+    setNotes(res.data);
+  };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  const handleNoteAdded = (newNote) => {
+    setNotes(prev => [newNote, ...prev]);
+  };
+
   return (
-    <div>
-      <h1>Welcome to Notes Manager</h1>
-      <div className="bg-blue-500 text-white text-3xl font-bold p-6">
-      ✅ Tailwind CSS is working!
-    </div>
-      <NotesList/>
+    <div className="min-h-screen bg-gray-100 p-6">
+      <AddNoteForm onNoteAdded={handleNoteAdded} />
+      <NotesList notes={notes} />
     </div>
   );
 }
